74e6ecb8b2aa66cb71cb4b3c112182d44a40217e
JankledsonRodriguez <jankledson.rodrigues@gmail.com>
2026-09-17 09:27:51 -0300

    Raise footer wave height and curve coordinates so it appears more prominently on the screen

--- src/components/common/Footer.tsx
+++ src/components/common/Footer.tsx
@@ -2,21 +2,21 @@ import React from 'react';
 
 export const Footer: React.FC = () => {
   return (
-    <footer className="w-full mt-auto overflow-hidden relative pt-8 pointer-events-none">
+    <footer className="w-full mt-auto overflow-hidden relative pt-10 pointer-events-none">
       <svg
         xmlns="http://www.w3.org/2000/svg"
         viewBox="0 0 1440 200"
-        className="w-full h-20 sm:h-28 block"
+        className="w-full h-32 sm:h-40 block"
         preserveAspectRatio="none"
       >
         {/* Linha de destaque Laranja */}
         <path
-          d="M0,30 C400,100 1000,0 1440,40 L1440,200 L0,200 Z"
+          d="M0,10 C400,90 1000,-10 1440,30 L1440,200 L0,200 Z"
           fill="#ff8928"
         />
         {/* Onda Azul Marinho */}
         <path
-          d="M0,50 C400,120 1000,20 1440,60 L1440,200 L0,200 Z"
+          d="M0,35 C400,115 1000,15 1440,55 L1440,200 L0,200 Z"
           fill="#002747"
         />
       </svg>
